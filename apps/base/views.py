848a94643da67f91f77e8c5a282c482203d4d69c
katia <@Keyt92>
2023-09-22 14:59:40 +0200

    base

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from faker import Faker
+
+faker = Faker()
 
 
 def index(request):
@@ -8,5 +11,31 @@ def index(request):
         context={
             "greetings_text": " Hello! ",
             "title": "Home page",
+        },
+    )
+
+
+def about_us_page(request):
+    return render(
+        request=request,
+        template_name="base/about_us_page.html",
+        context={
+            "greetings_text": "Information about us",
+            "title": "About us",
+        },
+    )
+
+
+def contacts_page(request):
+    user_name = faker.first_name()
+    phone = faker.phone_number()
+    email_domain = faker.domain_name()
+    return render(
+        request=request,
+        template_name="base/contacts_page.html",
+        context={
+            "greetings_text": f"Our contacts: phone number: {phone}  email: {user_name}@{email_domain}"
+            f"  address: Ukraine, Kyiv",
+            "title": "Contacts",
         },
     )
